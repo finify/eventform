266ab4dc5e02efe297713497c932b7560e91eb57
finify <ifeanyiizuegbu@yahoo.com>
2023-03-27 15:15:25 +0100

    fixing mail

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -93,11 +93,21 @@ class Api extends \Controller
 
                     $message = wordwrap($message, 70, "\r\n");
 
+                    // Always set content-type when sending HTML email
+                    $headers = "MIME-Version: 1.0" . "\r\n";
+                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+                    // More headers
+                    $headers .= 'From: <support@invite.brandco.tech>' . "\r\n";
+                    $headers .= 'Cc: ifeanyiizuegbu@yahoo.com' . "\r\n";
+
+                    mail($to,$subject,$message,$headers);
+
                     $data = [
                         'status'=> 'inserted'
                     ];
 
-                    mailto($to,$subject,$message);
+                    // mailto($to,$subject,$message);
                 }
             }
 
@@ -166,11 +176,21 @@ class Api extends \Controller
 
                     $message = wordwrap($message, 70, "\r\n");
 
+                    // Always set content-type when sending HTML email
+                    $headers = "MIME-Version: 1.0" . "\r\n";
+                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+                    // More headers
+                    $headers .= 'From: <support@invite.brandco.tech>' . "\r\n";
+                    $headers .= 'Cc: ifeanyiizuegbu@yahoo.com' . "\r\n";
+
+                    mail($to,$subject,$message,$headers);
+
                     $data = [
                         'status'=> 'inserted'
                     ];
 
-                    mailto($to,$subject,$message);
+                    // mailto($to,$subject,$message);
                 }
             }
 
