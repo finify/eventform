14fffe6e10b09e167ffe6ea4881f95e2c5360aa1
finify <ifeanyiizuegbu@yahoo.com>
2023-03-27 15:04:29 +0100

    adding loader

--- app/Views/Home/form.php
+++ app/Views/Home/form.php
@@ -186,6 +186,10 @@ backdrop-filter: blur(7.4px);
           <span id="submit_new">Submit</span>
         </fieldset>
         <fieldset>
+        <center>
+            <div class="loader"></div>
+            
+          </center>
                 <svg class="success" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
                     <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
                     <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>
@@ -316,6 +320,7 @@ backdrop-filter: blur(7.4px);
 
                     if(confirmed1 && confirmed2){
                         $("#submit_btn").click();
+                        $(".loader").toggle();
                     }
 
                     $.ajax({
@@ -325,6 +330,7 @@ backdrop-filter: blur(7.4px);
                             success: function (result) {
                                 if (result != "") {
 
+                                    $(".loader").toggle();
                                    
                                     let data = JSON.parse(JSON.stringify(result));
                                     console.log(data);
